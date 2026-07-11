95d5cea1754c1e2841cac700bcd507e8712d7df4
Joshua J <jj4440024@gmail.com>
2026-07-11 10:56:11 +0530

    View page created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddStudent from './components/AddStudent'
 import SearchStudent from './components/SearchStudent'
+import ViewAll from './components/ViewAll'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <AddStudent />
       <SearchStudent />
+      <ViewAll />
     </>
   )
 }
